b52c776ef4f718ea8c8e69137f58c1838d8b1771
ggambopce <ggambopce@gmail.com>
2024-11-21 13:05:39 +0900

    공통 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,31 @@ import './App.css';
 import Home from './pages/Home';
 import New from './pages/New';
 import Post from './pages/Post';
+import Edit from './pages/Edit';
 import Notfound from './pages/Notfound';
+
+import Button from './components/Button';
+import Header from './components/Header';
 
 
 // 1. "/": 모든 토론을 조회하는 Home페이지
 // 2. "/newpost": 새로운 토론을 작성하는 New페이지
 // 3. "/detailpost": 토론을 상세히 조회하는 Post페이지
+// 4. "/updatepost": 기존 토론을 수정하는 Edit페이지
 function App() { 
   return (
     <>
-      <div>
-        <Link to={"/"}>Home</Link>
-      </div>
+      <Header 
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+       
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/newpost" element={<New />}/>
         <Route path="/detailpost/:id" element={<Post />}/>
+        <Route path="/updatepost/:id" element={<Edit />}/>
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>

c01715c4ed6b850cb7016b0d42ba8e3a5b2a495c
ggambopce <ggambopce@gmail.com>
2024-11-21 15:17:15 +0900

    data,기능 Context생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route} from 'react-router-dom';
-import { useReducer, useRef } from 'react';
+import { useReducer, useRef, createContext } from 'react';
 import './App.css';
 import Home from './pages/Home';
 import New from './pages/New';
@@ -43,7 +43,8 @@ function reducer(state, action) {
   }
 }
 
-
+const PostStateContext = createContext();
+const PostDispatchContext = createContext();
 // 1. "/": 모든 토론을 조회하는 Home페이지
 // 2. "/newpost": 새로운 토론을 작성하는 New페이지
 // 3. "/detailpost": 토론을 상세히 조회하는 Post페이지
@@ -85,13 +86,24 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/newpost" element={<New />}/>
-        <Route path="/detailpost/:id" element={<Post />}/>
-        <Route path="/updatepost/:id" element={<Edit />}/>
-        <Route path="*" element={<Notfound />} />
-      </Routes>
+      <PostStateContext.Provider value={data}>
+        <PostDispatchContext.Provider
+          value={{
+            onCreate,
+            onUpdate,
+            onDelete,
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/newpost" element={<New />}/>
+            <Route path="/detailpost/:id" element={<Post />}/>
+            <Route path="/updatepost/:id" element={<Edit />}/>
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+        </PostDispatchContext.Provider>
+      </PostStateContext.Provider>
+  
     </>
     
   )

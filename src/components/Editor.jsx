89a68d193a35755694dc6c03a6502aa8056285d3
ggambopce <ggambopce@gmail.com>
2024-11-24 13:05:12 +0900

    작성 완료 기능 구현

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,19 +1,51 @@
+
 import "./Editor.css";
 import Button from "./Button";
+import { useState } from "react";
 
-const Editor = () => {
+const Editor = ({onSubmit}) => {
+    const [input, setInput] = useState({
+        postTitle: "",
+        postContent: "",
+    });
+
+    const onChangeInput = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+
+        setInput({
+            ...input,
+            [name]: value,
+        })
+    }
+
+    const onClickSubmitButton = () => {
+        onSubmit(input);
+    }
+
     return( 
     <div className="Editor">
         <section className="post_section">
             <h4>토론 주제</h4>
-            <input placeholder="나누고 싶은 주제를 작성해주세요" />
+            <input 
+                name="postTitle"
+                value={input.postTitle}
+                onChange={onChangeInput}
+                placeholder="나누고 싶은 주제를 작성해주세요" />
             <h4>토론 내용</h4>
-            <textarea placeholder="주제에 대한 내용과 RED와 BLUE의 기준을 작성해주세요" /> 
+            <textarea
+                name="postContent"
+                value={input.postContent}
+                onChange={onChangeInput}
+                placeholder="주제에 대한 내용과 RED와 BLUE의 기준을 작성해주세요" /> 
 
         </section>
         <section className="button_section">
             <Button text={"취소하기"} />
-            <Button text={"작성완료"} type={"POSITIVE"} />
+            <Button 
+                onClick={onClickSubmitButton}
+                text={"작성완료"} 
+                type={"POSITIVE"} />
 
         </section>
     </div>

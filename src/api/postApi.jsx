6e5fd14a5e2992ec825985e6a81ab6440f1c8843
ggambopce <ggambopce@gmail.com>
2024-11-25 16:49:02 +0900

    axios설정 완료

---
+++ src/api/postApi.jsx
@@ -0,0 +1,22 @@
+import axios from "axios"
+
+
+export const API_SERVER_HOST = 'http://localhost:8080'
+
+const prefix = `${API_SERVER_HOST}/api` 
+
+export const getOne = async(id) => {
+    const res = await axios.get(`${prefix}/user/posts/${id}`)
+
+    return res.data
+}
+
+export const getPosts = async(pageParam) => {
+
+    const {page, size} = pageParam
+
+    const res = await axios.get(`${prefix}/posts`, {params:{page,size}})
+
+    return res.data
+}
+
